4eec01aa6244f918ab572d5f4d4ed641663a50da
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-09-03 11:37:06 +0900

    ビューファイルの追加

---
+++ Gurumemo/Views/RessetPassword.swift
@@ -0,0 +1,18 @@
+//
+//  RessetPassword.swift
+//  Gurumemo
+//
+//  Created by 水元太陽 on 2024/09/03.
+//
+
+import SwiftUI
+
+struct RessetPassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RessetPassword()
+}
