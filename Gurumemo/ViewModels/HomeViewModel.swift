a8ec403d57dad5ba9dc6d1103aa276475eb94aba
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-09-03 11:57:07 +0900

    ログアウトボタンの実装

---
+++ Gurumemo/ViewModels/HomeViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewModel.swift
+//  Gurumemo
+//
+//  Created by 水元太陽 on 2024/09/03.
+//
+
+import Foundation
+import FirebaseAuth
+
+class HomeViewModel: ObservableObject {
+  @Published var isLogout:Bool = false
+  
+  init() {
+    //    インスタンスが生成されたら認証情報を確認
+    observeAuthChanges()
+  }
+  
+  private func observeAuthChanges() {
+    Auth.auth().addStateDidChangeListener { [weak self] _, user in
+      DispatchQueue.main.async {
+        self?.isLogout = user != nil
+      }
+    }
+  }
+  //  ログアウトのメソッド
+  func signOut() {
+    do {
+      try Auth.auth().signOut()
+      isLogout = true
+    } catch _ as NSError {
+      print("エラー")
+    }
+  }
+}
--- Gurumemo/Views/Home.swift
+++ Gurumemo/Views/Home.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct Home: View {
-    var body: some View {
-        Text("Hello, World!")
+  @StateObject var homeViewModel = HomeViewModel()
+  var body: some View {
+    NavigationStack {
+      VStack {
+        HStack {
+          Spacer()
+          Button(action: {
+            homeViewModel.signOut()
+            print(homeViewModel.isLogout)
+          }) {
+            Image(systemName: AppConstant.logoutIcon)
+              .font(.system(size: 25))
+              .padding()
+          }
+        }
+        VStack {
+        }
+        .padding()
+        Spacer()
+      }
+      NavigationLink(
+        destination: Login().toolbar(.hidden),
+        isActive: Binding(
+          get: { homeViewModel.isLogout },
+          set: { homeViewModel.isLogout = $0 }
+        ),
+        label: {
+          EmptyView()
+        })
     }
+  }
 }
 
 #Preview {
-    Home()
+  Home()
 }
