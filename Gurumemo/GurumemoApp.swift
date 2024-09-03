df65512745372da35684a19c1a917ad2897acf44
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-09-03 10:26:30 +0900

    Initial Commit

---
+++ Gurumemo/GurumemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GurumemoApp.swift
+//  Gurumemo
+//
+//  Created by 水元太陽 on 2024/09/03.
+//
+
+import SwiftUI
+
+@main
+struct GurumemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
