de1d0485297eec343b0f70ee2c14ab872c4056e4
Taiyo Mzumoto <154989695+Taiyo86-create@users.noreply.github.com>
2024-09-04 17:47:02 +0900

    現在位置を取得

--- Gurumemo/ViewModels/LocationManager.swift
+++ Gurumemo/ViewModels/LocationManager.swift
@@ -11,47 +11,33 @@ import MapKit
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
   
-  private var cllLocationManager = CLLocationManager()
+  let cllLocationManager = CLLocationManager()
   
-  @Published var region = MKCoordinateRegion(
-    center: CLLocationCoordinate2D(latitude: 35.682839, longitude: 139.759455),
-    span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-  )
+  @Published var region = MKCoordinateRegion()
   
   override init() {
     super.init()
     cllLocationManager.delegate = self
-    cllLocationManager.desiredAccuracy = kCLLocationAccuracyBest
     cllLocationManager.requestWhenInUseAuthorization()
+    cllLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+    cllLocationManager.distanceFilter = 2
+    cllLocationManager.startUpdatingLocation()
   }
   
-  private func checkLocationAuthorization() {
-    let status = CLLocationManager.authorizationStatus()
-    switch status {
-    case .notDetermined:
-      // まだユーザーに許可を求めていない状態
-      cllLocationManager.requestWhenInUseAuthorization()
-    case .restricted, .denied:
-      // 位置情報の利用が制限されているか、拒否されている状態
-      print("Location access restricted or denied.")
-    case .authorizedAlways, .authorizedWhenInUse:
-      // 常に利用するか、アプリ使用中のみ利用を許可された状態
-      print("Location access granted.")
-      cllLocationManager.startUpdatingLocation() // 位置情報の更新を開始
-    @unknown default:
-      // 将来の新しいステータスへの対応
-      print("Unknown location status.")
+  private func locationManager(
+    _ manager: CLLocationManager,
+    didUpdateLocations locations: [CLLocation])
+  {
+    locations.last.map {
+      let center = CLLocationCoordinate2D(
+        latitude: $0.coordinate.latitude,
+        longitude: $0.coordinate.longitude
+      )
+      region = MKCoordinateRegion(
+        center: center,
+        latitudinalMeters: 100.0
+        longitudinalMeters: 100.0
+      )
     }
-  }
-  
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    guard let location = locations.last else { return }
-    DispatchQueue.main.async {
-      self.region.center = location.coordinate
-    }
-  }
-  
-  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    print("Failed to find user's location: \(error.localizedDescription)")
   }
 }
