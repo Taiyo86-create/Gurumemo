f1987fd9848245eb6a063bc4f81e97c0ac4fe3a4
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-09-05 21:46:42 +0900

    LocationManagerの記述

--- Gurumemo/ViewModels/LocationManager.swift
+++ Gurumemo/ViewModels/LocationManager.swift
@@ -4,3 +4,32 @@
 //
 //  Created by 水元太陽 on 2024/09/03.
 //
+import MapKit
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+  let manager = CLLocationManager()
+  
+  @Published var region = MKCoordinateRegion()
+  
+  override init() {
+    super.init()
+    manager.delegate = self
+    manager.requestWhenInUseAuthorization()
+    manager.desiredAccuracy = kCLLocationAccuracyBest
+    manager.distanceFilter = 2
+    manager.startUpdatingLocation()
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    locations.last.map {
+      let center = CLLocationCoordinate2D(
+        latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude
+      )
+      region = MKCoordinateRegion(
+        center: center,
+        latitudinalMeters: 100.0,
+        longitudinalMeters: 100.0
+      )
+    }
+  }
+}
