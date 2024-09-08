fe0c63d78a2c2291f2f2672af1c626819099911f
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-09-08 18:26:13 +0900

    Modelの記述

--- Gurumemo/Models/SearchMapModel.swift
+++ Gurumemo/Models/SearchMapModel.swift
@@ -6,4 +6,22 @@
 //
 
 import Foundation
+import RealmSwift
 
+class SearchMapModel: Object {
+  @Persisted var latitude: Double = 0.0
+  @Persisted var longitude: Double = 0.0
+  
+  convenience init(latitude: Double, longitude: Double) {
+    self.init()
+    self.latitude = latitude
+    self.longitude = longitude
+  }
+  
+  func saveRange() {
+    let realm = try! Realm()
+    try! realm.write {
+      realm.add(self)
+    }
+  }
+}
